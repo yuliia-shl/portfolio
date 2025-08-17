1bb256c7417d173700da210146263e8464b49e56
Yuli <yu.expert@gmail.com>
2025-08-18 00:12:05 +0300

    style: update header styles and add icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,44 @@
 const Header = () => {
   return (
     <header>
-      <a href="/" className="uppercase">
-        YULIIA SHLIAPTSEVA
-      </a>
-      <nav>
-        <ul>
-          <li>
-            <a href="#projects">Work</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="container mx-auto px-[5%] flex justify-between">
+        <a
+          href="/"
+          aria-label="На головну"
+          className="font-second text-[28px]/[100%] lg:text-[32px]/[150%] uppercase tracking-[-0.01em] py-6"
+        >
+          YULIIA SHLIAPTSEVA
+        </a>
+        <nav className="flex items-center justify-center">
+          <button
+            aria-label="Open menu"
+            /* aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}> */
+            aria-expanded="false"
+            className="md:hidden "
+          >
+            <svg aria-hidden="true" className="w-11 h-11 stroke-silver-300">
+              <use href="/icons.svg#icon-burger" />
+            </svg>
+          </button>
+          <ul className="hidden md:flex items-center gap-[3vw] font-third leading-[150%] font-medium tracking-[-0.03em] ">
+            <li>
+              <a href="#projects" className="block py-6">
+                Work
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="block py-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="block py-6">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
